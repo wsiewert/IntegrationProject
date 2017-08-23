e7bdf310042442b1e0162b67e61d98144eae1a6e
John Lor <j_lor@hotmail.com>
2017-08-23 15:15:25 -0500

    test

--- IntegrationProject/IntegrationProject/Controllers/HomeController.cs
+++ IntegrationProject/IntegrationProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace IntegrationProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page blah.";
 
             return View();
         }
