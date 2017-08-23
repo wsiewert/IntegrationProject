f90106b2a6f5804ffca9116a724a09f4030b6947
wsiewert <wsiewert94@gmail.com>
2017-08-23 15:02:21 -0500

    added partial volunteerEvent Model

---
+++ IntegrationProject/IntegrationProject/Models/VolunteerEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IntegrationProject.Models
+{
+    public class VolunteerEvent
+    {
+        [Key]
+        public int ID { get; set; }
+    }
+}
